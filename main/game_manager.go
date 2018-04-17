7a84b7067ec45cbb0f74ebefdcae48a659b19154
Cenk BAYKUL <cenkbaykul@gmail.com>
2018-04-17 15:50:56 +0300

    hud and player complete

--- main/game_manager.go
+++ main/game_manager.go
@@ -31,3 +31,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "player_factory"
+  type: "factory"
+  data: "prototype: \"/player/player.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
