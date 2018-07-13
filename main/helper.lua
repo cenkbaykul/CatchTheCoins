74d6f03c0764d1c2bb306c7d8d26678720d7c608
Cenk BAYKUL <cenkbaykul@gmail.com>
2018-07-13 10:59:10 +0300

    some optimizations added

--- main/helper.lua
+++ main/helper.lua
@@ -1,28 +1,22 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+
 M = {}
 
 function M.get_screen_widht(self)
-	local width = tonumber(sys.get_config("display.width"))
-	return width
+	return tonumber(sys.get_config("display.width"))
 end
 
-function M.get_screen_height(self)
-	local height = tonumber(sys.get_config("display.height"))
-	return height
+function M.get_screen_height()
+	return tonumber(sys.get_config("display.height"))
 end
 
-function M.get_screensize(self)
+function M.get_screensize()
 	local width = tonumber(sys.get_config("display.width"))
 	local height = tonumber(sys.get_config("display.height"))
-	local screensize = vmath.vector3(width,height,0)
-	return screensize
+	return vmath.vector3(width, height, 0)
 end
-function M.get_random_seed(self)
-	local seed = os.time() + (os.clock()*1000000) + math.random(0, 65535)
-	return seed
+
+function M.get_random_seed()
+	return os.clock() + math.sin(0.5) / math.random()
 end
 
 return M
